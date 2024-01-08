1cb8f8bf47db43669a16c07970ba9865070a0b70
Shyam Prag <76562906+Shyam-Prag@users.noreply.github.com>
2024-01-08 15:25:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,35 @@
+#```
+
 import json
 import boto3 
 import time
 
-def lambda_handler():
+def main():
     client = boto3.client('apigateway')
-    
     response = client.get_rest_apis()
-    
     api_ids = []
-    
+    list_of_deleted_rest_apis = []
     list_of_items = response['items']
     
     for api_id in list_of_items:
-        
         a_single_api_id = api_id['id']
-        
         api_ids.append(a_single_api_id)
-    
+
     print(api_ids)    
-    
+
     for delete_api in api_ids:
         print(delete_api)
-        if delete_api in ('cundn3cojf', 'fnrty94jud', 'pxvwy3qnk3','ha6gmyhn64','ip07qpld3l'): #enter a list of API ID's which you DO NOT want to delete
+        if delete_api in ('xxxxxxxx', 'xxxxxxxx', 'xxxxxxxx','xxxxxxxx','xxxxxxxx'): #enter a list of API ID's which you DO NOT want to delete
             print(f'doing nothing.. not deleting {delete_api}')
         
         else:    
-            print('sleeping 35 seconds')    
+            print('sleeping 35 seconds')    #sleeping 35 seconds due to 30 second TPS limit.
             time.sleep(35)
             client.delete_rest_api(restApiId = delete_api)
+            list_of_deleted_rest_apis.append(delete_api)
             print(f'deleted api -> {delete_api}')
             
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hellow world')
-    }
+    return list_of_deleted_rest_apis
 
-lambda_handler()
+main()
+#```
